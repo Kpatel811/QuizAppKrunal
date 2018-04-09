2286e19d7a7b1b4b8331aadeb8f0e41cdedbc147
Kpatel811 <37032732+Kpatel811@users.noreply.github.com>
2018-04-09 11:34:46 -0500

    updates
    Updated background, colors, strings dimens, and java updated to show global variables.

--- app/src/main/java/com/example/android/quizappkrunal/MainActivity.java
+++ app/src/main/java/com/example/android/quizappkrunal/MainActivity.java
@@ -4,6 +4,7 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -30,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
     RadioButton q5_option3;
     RadioButton q6_option2;
     RadioButton q7_option2;
+    RadioGroup q3radioGroup;
+    RadioGroup q4radioGroup;
+    RadioGroup q5radioGroup;
+    RadioGroup q6radioGroup;
+    RadioGroup q7radioGroup;
 
 
     @Override
@@ -37,69 +43,93 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText q1Field = (EditText) findViewById(R.id.q1_option1);
+        q1Field = (EditText) findViewById(R.id.q1_option1);
+
+        q1Field = (EditText) findViewById(R.id.q1_option1);
+
+        q2_option1 = (CheckBox) findViewById(R.id.q2_option1);
+        q2_option2 = (CheckBox)findViewById(R.id.q2_option2);
+        q2_option3 = (CheckBox) findViewById(R.id.q2_option3);
+        q2_option4 = (CheckBox) findViewById(R.id.q2_option4);
+        q2_option5 = (CheckBox) findViewById(R.id.q2_option5);
+        q2_option6 = (CheckBox) findViewById(R.id.q2_option6);
+        q2_option7 = (CheckBox) findViewById(R.id.q2_option7);
+        q3_option1 = (RadioButton) findViewById(R.id.q3_option1);
+        q4_option3 = (RadioButton) findViewById(R.id.q4_option3);
+        q5_option3 = (RadioButton) findViewById(R.id.q5_option3);
+        q6_option2 = (RadioButton) findViewById(R.id.q6_option2);
+        q7_option2 = (RadioButton) findViewById(R.id.q7_option2);
+        q3radioGroup = (RadioGroup)findViewById(R.id.question_3_choices);
+        q4radioGroup = (RadioGroup)findViewById(R.id.question_4_choices);
+        q5radioGroup = (RadioGroup)findViewById(R.id.question_5_choices);
+        q6radioGroup = (RadioGroup)findViewById(R.id.question_6_choices);
+        q7radioGroup = (RadioGroup)findViewById(R.id.question_7_choices);
+
     }
+
+
+
 
     public void submitQuiz(View view){
 
         //EditText q1Field = (EditText) findViewById(R.id.q1_option1);
-        q1Field = (EditText) findViewById(R.id.q1_option1);
+        //q1Field = (EditText) findViewById(R.id.q1_option1);
         String q1Choice= q1Field.getText().toString();
 
         //CheckBox q2_option1 = (CheckBox) findViewById(R.id.q2_option1);
-        q2_option1 = (CheckBox) findViewById(R.id.q2_option1);
+        //q2_option1 = (CheckBox) findViewById(R.id.q2_option1);
         boolean q2choice1= q2_option1.isChecked();
 
         //CheckBox q2_option2 = (CheckBox) findViewById(R.id.q2_option2);
-        q2_option2 = (CheckBox)findViewById(R.id.q2_option2);
+        //q2_option2 = (CheckBox)findViewById(R.id.q2_option2);
         boolean q2choice2= q2_option2.isChecked();
 
         //CheckBox q2_option3= (CheckBox) findViewById(R.id.q2_option3);
-        q2_option3= (CheckBox) findViewById(R.id.q2_option3);
+        //q2_option3= (CheckBox) findViewById(R.id.q2_option3);
         boolean q2choice3= q2_option3.isChecked();
 
         //CheckBox q2_option4= (CheckBox) findViewById(R.id.q2_option4);
-        q2_option4= (CheckBox) findViewById(R.id.q2_option4);
+       // q2_option4= (CheckBox) findViewById(R.id.q2_option4);
         boolean q2choice4= q2_option4.isChecked();
 
         //CheckBox q2_option5= (CheckBox) findViewById(R.id.q2_option5);
-        q2_option5= (CheckBox) findViewById(R.id.q2_option5);
+        //q2_option5= (CheckBox) findViewById(R.id.q2_option5);
         boolean q2choice5= q2_option5.isChecked();
 
         //CheckBox q2_option6= (CheckBox) findViewById(R.id.q2_option6);
-        q2_option6= (CheckBox) findViewById(R.id.q2_option6);
+       // q2_option6= (CheckBox) findViewById(R.id.q2_option6);
         boolean q2choice6= q2_option6.isChecked();
 
         //CheckBox q2_option7= (CheckBox) findViewById(R.id.q2_option7);
-        q2_option7= (CheckBox) findViewById(R.id.q2_option7);
+        //q2_option7= (CheckBox) findViewById(R.id.q2_option7);
         boolean q2choice7= q2_option7.isChecked();
 
 
         //RadioButton q3_option1= (RadioButton) findViewById(R.id.q3_option1);
-        q3_option1= (RadioButton) findViewById(R.id.q3_option1);
+        //q3_option1= (RadioButton) findViewById(R.id.q3_option1);
         boolean q3choice= q3_option1.isChecked();
 
 
         //RadioButton q4_option3= (RadioButton) findViewById(R.id.q4_option3);
-        q4_option3= (RadioButton) findViewById(R.id.q4_option3);
+        //q4_option3= (RadioButton) findViewById(R.id.q4_option3);
         boolean q4choice= q4_option3.isChecked();
 
         //RadioButton q5_option3= (RadioButton) findViewById(R.id.q5_option3);
-        q5_option3= (RadioButton) findViewById(R.id.q5_option3);
+        //q5_option3= (RadioButton) findViewById(R.id.q5_option3);
         boolean q5choice= q5_option3.isChecked();
 
         //RadioButton q6_option2= (RadioButton) findViewById(R.id.q6_option2);
-        q6_option2= (RadioButton) findViewById(R.id.q6_option2);
+        //q6_option2= (RadioButton) findViewById(R.id.q6_option2);
         boolean q6choice= q6_option2.isChecked();
 
         //RadioButton q7_option2= (RadioButton) findViewById(R.id.q7_option2);
-        q7_option2= (RadioButton) findViewById(R.id.q7_option2);
+        //q7_option2= (RadioButton) findViewById(R.id.q7_option2);
         boolean q7choice= q7_option2.isChecked();
 
 
        finalScore = calculateScore(q1Choice, q2choice1, q2choice2, q2choice3, q2choice4, q2choice5, q2choice6, q2choice7, q3choice, q4choice, q5choice, q6choice, q7choice);
 
-        Toast toast = Toast.makeText(getApplicationContext(), "Your FINAL SCORE is " + finalScore + "/7", Toast.LENGTH_LONG);
+        Toast toast = Toast.makeText(getApplicationContext(), getString((R.string.your_final_score)) + ' ' + finalScore  + getString(R.string.out_of), Toast.LENGTH_LONG);
         toast.show();
 
         Log.v("score","score: " + finalScore);
@@ -130,92 +160,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void resetQuiz (View view){
-
-       if(q1Field != null)
-           q1Field.setText("");
-       else{
-           q1Field = (EditText) findViewById(R.id.q1_option1);
-           q1Field.setText("");
-       }
-
-       if(q2_option1 != null)
-           q2_option1.setChecked(false);
-       else {
-           q2_option1 = (CheckBox) findViewById(R.id.q2_option1);
-           q2_option1.setChecked(false);
-       }
-
-       if(q2_option2 != null)
-           q2_option2.setChecked(false);
-       else {
-           q2_option2 = (CheckBox) findViewById(R.id.q2_option2);
-           q2_option2.setChecked(false);
-       }
-
-
-        if(q2_option3 != null)
-            q2_option3.setChecked(false);
-        else {
-            q2_option3 = (CheckBox) findViewById(R.id.q2_option3);
-            q2_option3.setChecked(false);
-        }
-
-        if(q2_option4 != null)
-            q2_option4.setChecked(false);
-        else {
-            q2_option4 = (CheckBox) findViewById(R.id.q2_option4);
-            q2_option4.setChecked(false);
-        }
-
-
-        if(q2_option5 != null)
-            q2_option5.setChecked(false);
-        else {
-            q2_option5 = (CheckBox) findViewById(R.id.q2_option5);
-            q2_option5.setChecked(false);
-        }
-
-
-        if(q2_option6 != null)
-            q2_option6.setChecked(false);
-        else {
-            q2_option6 = (CheckBox) findViewById(R.id.q2_option6);
-            q2_option6.setChecked(false);
-        }
-
-
-        if(q2_option7 != null)
+            q1Field.setText("");
+            q2_option1.setChecked(false);
             q2_option2.setChecked(false);
-        else {
-            q2_option7 = (CheckBox) findViewById(R.id.q2_option7);
+            q2_option3.setChecked(false);
+            q2_option4.setChecked(false);
+            q2_option5.setChecked(false);
+            q2_option6.setChecked(false);
             q2_option7.setChecked(false);
-        }
 
-
-        RadioGroup q3radioGroup = (RadioGroup)findViewById(R.id.question_3_choices);
+           //RadioGroup q3radioGroup = (RadioGroup)findViewById(R.id.question_3_choices);
         q3radioGroup.clearCheck();
 
-        RadioGroup q4radioGroup = (RadioGroup)findViewById(R.id.question_4_choices);
+        //RadioGroup q4radioGroup = (RadioGroup)findViewById(R.id.question_4_choices);
         q4radioGroup.clearCheck();
 
-        RadioGroup q5radioGroup = (RadioGroup)findViewById(R.id.question_5_choices);
+        //RadioGroup q5radioGroup = (RadioGroup)findViewById(R.id.question_5_choices);
         q5radioGroup.clearCheck();
 
-        RadioGroup q6radioGroup = (RadioGroup)findViewById(R.id.question_6_choices);
+        //RadioGroup q6radioGroup = (RadioGroup)findViewById(R.id.question_6_choices);
         q6radioGroup.clearCheck();
 
-        RadioGroup q7radioGroup = (RadioGroup)findViewById(R.id.question_7_choices);
+        //RadioGroup q7radioGroup = (RadioGroup)findViewById(R.id.question_7_choices);
         q7radioGroup.clearCheck();
 
-        Toast toast = Toast.makeText(getApplicationContext(), "Try Again!", Toast.LENGTH_LONG);
+        Toast toast = Toast.makeText(getApplicationContext(), getString((R.string.try_again)), Toast.LENGTH_LONG);
         toast.show();
-
 
     }
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
-
         // Check which radio button was clicked
         switch(view.getId()) {
             case R.id.q3_option1:
